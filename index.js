7561950f954c656f7e0f5e4afc02865ac6c388d2
Muhamad Iqbal Fauzi <94272637+MiqbalF2021@users.noreply.github.com>
2024-12-08 22:18:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://coffeess.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 
 //Register User
